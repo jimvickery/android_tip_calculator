1e7d9507924924d544b3d0c2b8a5e3b3150726fb
Jim Vickery <jimrv8@yahoo.com>
2018-01-25 22:09:44 -0600

    initial commit and completed

---
+++ app/src/main/java/com/murach/tipcalculator/TipCalculatorActivity.java
@@ -0,0 +1,17 @@
+package com.investprofits.jimvickery.tipcalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TipCalculatorActivity extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);     
+        
+        // Display the fragment as the main content
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new TipCalculatorFragment())
+                .commit();
+    }
+}
